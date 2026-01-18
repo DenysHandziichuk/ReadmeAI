7f344ee23ce41d85a13de1f33fc8a831ee97eacf
DenysHandziichuk <fleymidesign@gmail.com>
2026-01-18 15:40:40 -0500

    feat: refactor RepoPage to use RepoClient component and remove form

--- app/dashboard/[owner]/[repo]/page.tsx
+++ app/dashboard/[owner]/[repo]/page.tsx
@@ -1,3 +1,4 @@
+import RepoClient from "./repo-client";
 type Props = {
   params: Promise<{
     owner: string;
@@ -8,23 +9,5 @@ type Props = {
 export default async function RepoPage({ params }: Props) {
   const { owner, repo } = await params;
 
-  return (
-    <main className="p-8">
-      <h1 className="text-2xl font-bold mb-4">
-        {owner}/{repo}
-      </h1>
-
-      <form action="/api/generate" method="POST">
-        <input type="hidden" name="owner" value={owner} />
-        <input type="hidden" name="repo" value={repo} />
-
-        <button
-          type="submit"
-          className="px-4 py-2 bg-black text-white rounded"
-        >
-          Generate README
-        </button>
-      </form>
-    </main>
-  );
+  return <RepoClient owner={owner} repo={repo} />;
 }

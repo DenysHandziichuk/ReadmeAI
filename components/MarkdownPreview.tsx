f3b05a44488c7e65f16858bdd79a23da1148de25
DenysHandziichuk <fleymidesign@gmail.com>
2026-03-25 00:21:10 -0400

    feat: enhance MarkdownPreview to support raw HTML and sanitize input

--- components/MarkdownPreview.tsx
+++ components/MarkdownPreview.tsx
@@ -2,11 +2,18 @@
 
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
+import rehypeRaw from "rehype-raw";
+import rehypeSanitize from "rehype-sanitize";
 
 export default function MarkdownPreview({ content }: { content: string }) {
   return (
     <article className="markdown-body">
-      <ReactMarkdown remarkPlugins={[remarkGfm]}>{content}</ReactMarkdown>
+      <ReactMarkdown 
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw, rehypeSanitize]}
+      >
+        {content}
+      </ReactMarkdown>
     </article>
   );
 }
